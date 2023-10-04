5da0ffd21ef455cbe88f84685c2abf808ddb2d5b
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-04 04:28:49 +0200

    feat: make a meal card

--- src/components/molecules/Meal.tsx
+++ src/components/molecules/Meal.tsx
@@ -1,7 +1,20 @@
 import React from "react"
 
-export const Meal = () => {
+type mealProps = {
+    name: string,
+    category: string,
+    image: string,
+    duration: number
+}
+export const Meal = ({name, category, image, duration}: mealProps) => {
     return (
-        <div className="h-100 w-100 flex">Meal</div>
+        <div className=" bg-cover bg-center bg-[url('https://images.pexels.com/photos/3220617/pexels-photo-3220617.jpeg?auto=compress&cs=tinysrgb&w=1600')] flex min-w-[12.5rem] min-h-[18.75rem]  flex-col justify-end items-start gap-2 rounded-2xl shadow-m ">
+            <div className=" flex-grow w-full bg-gradient-to-t from-black to-transparent p-2 opacity-90 rounded-2xl flex flex-col justify-end">
+                <div>
+                    <span className="text-white text-base leading-6 font-bold">{name}</span>
+                    <span className="text-white text-base leading-6 font-normal">{duration} min</span>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/organisms/Meals.tsx
+++ src/components/organisms/Meals.tsx
@@ -4,7 +4,7 @@ import { Meal } from "../molecules/Meal"
 export const Meals = () => {
     return (
         <div className="flex items-start gap-2 self-stretch">
-            <Meal/>
+            <Meal name={"Hamburguer"} category={"Lunch"} image={'https://images.pexels.com/photos/3220617/pexels-photo-3220617.jpeg?auto=compress&cs=tinysrgb&w=1600'} duration={30}/>
         </div>
     )
 }
