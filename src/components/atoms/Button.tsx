20cae5d408f8585adcc7a1696e6f5d1dd6f6c733
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-04 04:54:18 +0200

    feat: make all the home page

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const Button = () => {
+    return(
+        <button className="flex justify-end items-center gap-3 bg-orange-600 text-white px-4 py-2 rounded-full">Explore</button>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { NavBar } from "../components/molecules/NavBar";
 import { SearchBar } from "../components/molecules/SearchBar";
 import { Pills } from "../components/organisms/Pills";
 import { Meals } from "../components/organisms/Meals";
+import { Button } from "../components/atoms/Button";
 export const Home = () => {
     return (
         <div className=" h-screen w-screen flex px-4 pt-2 pb4 flex-col bg-gray-100">
@@ -19,6 +20,25 @@ export const Home = () => {
                 <div className="flex flex-col gap-4 items-start self-stretch">
                     <Pills/>
                     <Meals/>
+                </div>
+                {/* popular meals title */}
+                <div className="flex items-start gap-2 self-stretch justify-between">
+                    <h1 className="text-base leading-6 font-semibold text-gray-700">Popular meals</h1>
+                    <a href="./menu" className="text-base leading-6 font-light text-orange-600"> see all</a>
+
+                </div>
+                {/* popular meals */}
+                <Meals/>
+                {/* something new */}
+                <div className="flex items-start gap-2 self-stretch flex-col">
+                    <h1 className="text-base leading-6 font-semibold text-gray-700">Something new</h1>
+                    <div className="flex p-2 justify-center items-center gap-2 flex-grow self-stretch rounded-2xl bg-gray-200">
+                        <span className="text-base leading-6 font-medium">
+                            We have alot of recomendations for you!
+                        </span>
+                        <Button/>
+
+                    </div>
                 </div>
 
             </div>
