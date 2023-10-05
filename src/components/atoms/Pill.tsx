6748e7c28f481c4a1d2fcfa8061f09ac7cf13ac1
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-05 23:48:47 +0200

    feat: add loader

--- src/components/atoms/Pill.tsx
+++ src/components/atoms/Pill.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 type PillProps = {
     category: string
     active?: boolean
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -91,42 +91,55 @@ export const Home = () => {
     }
     return (
         <div className=" h-screen w-screen flex px-4 pt-2 pb4 flex-col bg-gray-100">
-            <NavBar/>
-            {/* body */}
-            <div className="flex py-4 flex-col items-start gap-4 flex-grow self-stretch">
-                {/* header */}
-                <div className="flex flex-col items-start gap-2 self-stretch">
-                    <h1 className="text-sm leading-5 font-light text-center text-orange-600">Good morning</h1>
-                    <p className="text-gray-700 text-base leading-6 font-normal w-1/2">What would you like to cook today?</p>
-                    <SearchBar/>
-                </div>
-                {/* meals by categories */}
-                <div className="flex flex-col gap-4 items-start self-stretch">
-                    <Pills categories={ListCategories} addSelectedCategory={toggleCategory} />
-                    <Meals meals={meals.filter(meal => {
-                            return meal.categories.some(category => categories.includes(category)) || categories.length === 0
-                        })}/>
-                </div>
-                {/* popular meals title */}
-                <div className="flex items-start gap-2 self-stretch justify-between">
-                    <h1 className="text-base leading-6 font-semibold text-gray-700">Popular meals</h1>
-                    <a href="./menu" className="text-base leading-6 font-light text-orange-600"> see all</a>
-                </div>
-                {/* popular meals */}
-                <Meals meals={popularMeals}/>
-                {/* something new */}
-                <div className="flex items-start gap-2 self-stretch flex-col">
-                    <h1 className="text-base leading-6 font-semibold text-gray-700">Something new</h1>
-                    <div className="flex p-2 justify-center items-center gap-2 flex-grow self-stretch rounded-2xl bg-gray-200">
-                        <span className="text-base leading-6 font-medium text-gray-500">
-                            We have alot of recomendations for you!
-                        </span>
-                        <Button/>
+            { popularMeals.length === 0 || meals.length === 0  ? 
+                <div className="flex flex-col items-center justify-center h-screen w-screen">
+                    <span className="relative flex   justify-center items-center">
+                        <span className="animate-ping absolute inline-flex h-8 w-8 rounded-full bg-orange-400 opacity-75"></span>
+                        <span className="relative inline-flex rounded-full h-8 w-8 bg-orange-500"></span>
+                    </span>
+                    <p className="text-2xl leading-6 font-semibold text-orange-500">Loading</p>
 
+                </div>
+             : 
+             <>
+                <NavBar/>
+                {/* body */}
+                <div className="flex py-4 flex-col items-start gap-4 flex-grow self-stretch">
+                    {/* header */}
+                    <div className="flex flex-col items-start gap-2 self-stretch">
+                        <h1 className="text-sm leading-5 font-light text-center text-orange-600">Good morning</h1>
+                        <p className="text-gray-700 text-base leading-6 font-normal w-1/2">What would you like to cook today?</p>
+                        <SearchBar/>
                     </div>
-                </div>
+                    {/* meals by categories */}
+                    <div className="flex flex-col gap-4 items-start self-stretch">
+                        <Pills categories={ListCategories} addSelectedCategory={toggleCategory} />
+                        <Meals meals={meals.filter(meal => {
+                                return meal.categories.some(category => categories.includes(category)) || categories.length === 0
+                            })}/>
+                    </div>
+                    {/* popular meals title */}
+                    <div className="flex items-start gap-2 self-stretch justify-between">
+                        <h1 className="text-base leading-6 font-semibold text-gray-700">Popular meals</h1>
+                        <a href="./menu" className="text-base leading-6 font-light text-orange-600"> see all</a>
+                    </div>
+                    {/* popular meals */}
+                    <Meals meals={popularMeals}/>
+                    {/* something new */}
+                    <div className="flex items-start gap-2 self-stretch flex-col">
+                        <h1 className="text-base leading-6 font-semibold text-gray-700">Something new</h1>
+                        <div className="flex p-2 justify-center items-center gap-2 flex-grow self-stretch rounded-2xl bg-gray-200">
+                            <span className="text-base leading-6 font-medium text-gray-500">
+                                We have alot of recomendations for you!
+                            </span>
+                            <Button/>
 
-            </div>
+                        </div>
+                    </div>
+
+                </div>
+             </>
+             }
         </div>
     );
 };
