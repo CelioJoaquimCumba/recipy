fa2866b2926e99cc4e87067c18296f91aac0f2f7
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-09 18:27:54 +0200

    refactor: change typo on attributes

--- src/components/molecules/NavBar.tsx
+++ src/components/molecules/NavBar.tsx
@@ -8,7 +8,7 @@ export const NavBar = ({back=false}: NavBarProps) => {
             <div className="flex flex-row gap-2 items-end flex-grow">
                 {back ? (
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="gray" className="w-6 h-6">
-                        <path fill-rule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clip-rule="evenodd" />
+                        <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clipRule="evenodd" />
                     </svg>
                 ) : ""}
                 <h1 className="text-gray-900 text-lg leading-7 font-semibold">Recipy</h1>
