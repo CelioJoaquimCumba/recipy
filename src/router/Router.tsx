0bca77eae4ffe7d4d1f0f84749c3db3d008374fc
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-09 18:25:44 +0200

    refactor: change the identifier to id

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
                 element: <Home/>
             },
             {
-                path: "meal/:mealUri",
+                path: "meal/:id",
                 element: <Meal/>
             },
             {
