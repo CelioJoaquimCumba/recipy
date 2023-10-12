a560b75eb881b1589e767feb2ba69d1d624b45aa
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-12 08:36:16 +0200

    refactor: change padding and truncate text

--- src/components/molecules/NavBar.tsx
+++ src/components/molecules/NavBar.tsx
@@ -29,11 +29,11 @@ export const NavBar = () => {
         };
     }, [prevScrollPos]);
     return(
-        <div className={`flex flex-row bg-gray-50 rounded-b-2xl  w-full px-2 py-4 justify-between items-end sticky top-0 z-10 transition-transform duration-500 ${scrollingUp ? 'translate-y-0' : '-translate-y-full'}`}>
+        <div className={`flex flex-row bg-gray-50 rounded-b-2xl  w-full p-4 justify-between items-end sticky top-0 z-10 transition-transform duration-500 ${scrollingUp ? 'translate-y-0' : '-translate-y-full'}`}>
             <div className="flex flex-row gap-2 items-end flex-grow">
                 <h1 className="text-gray-900 text-lg leading-7 font-semibold"><Link to="/">Recipy</Link></h1>
             </div>
-            <h1 className="text-xs leading-4 font-normal text-gray-900">made by Celio Cumba</h1>
+            <h1 className="text-xs leading-4 font-normal text-gray-900 truncate">made by Celio Cumba</h1>
         </div>
     )
 }
