b67222c7b23430368ffb664ad19f2abeb31543a5
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-14 19:16:53 +0200

    refactor: remove scroll bar

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <div className='flex-grow hidden md:flex'>
         Laptop View is still in development, please use a mobile phone or a tablet for a better experience
       </div>
-      <div className='flex w-full h-full md:max-w-md md:min-w-sm bg overflow-auto bg-white md:rounded-2xl'>
+      <div className='flex w-full h-full md:max-w-md md:min-w-sm bg overflow-auto no-scrollbar bg-white md:rounded-2xl'>
         <RouterProvider router={router} fallbackElement={<div>Loading...</div>} />
       </div>
       <div className='flex-grow hidden md:flex'>
