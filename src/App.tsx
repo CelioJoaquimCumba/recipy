b6f7bfd239c4fa5be6a84a3599daf1aaf4af48ae
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-14 19:11:01 +0200

    refactor: rectify a typo

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import { router } from './router/Router';
 function App() {
   return (
     <div className="h-100 md:h-screen w-100 flex bg-gray-200 md:p-6">
-      <div className=' flex-grow hidden md:flex'>
-        Laptop VIew is still in development, please use a mobile phone or a tablet for a better experience
+      <div className='flex-grow hidden md:flex'>
+        Laptop View is still in development, please use a mobile phone or a tablet for a better experience
       </div>
       <div className='flex w-full h-full md:max-w-md md:min-w-sm bg overflow-auto bg-white md:rounded-2xl'>
         <RouterProvider router={router} fallbackElement={<div>Loading...</div>} />
       </div>
       <div className='flex-grow hidden md:flex'>
-        Laptop VIew is still in development, please use a mobile phone or a tablet for a better experience
+        Laptop View is still in development, please use a mobile phone or a tablet for a better experience
       </div>
     </div>
   );
